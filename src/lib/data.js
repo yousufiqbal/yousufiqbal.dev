d16a0f753cebe41a60eab26a5a119bfec988bd33
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-08-28 04:31:53 -0700

    proper single quotes

--- src/lib/data.js
+++ src/lib/data.js
@@ -11,14 +11,14 @@ export let benefits = [
     icon: 'ri:mail-send-line',
     above: '$ 0.00',
     title: 'Free Custom Email',
-    description: 'Like contact@mydomain.com, you get one free email of 5GB space. This helps you respond to messages that are sent from the ‘Contact Me’ page from your website. Just install email application on your phone and respond to emails on the go.'
+    description: `Like contact@mydomain.com, you get one free email of 5GB space. This helps you respond to messages that are sent from the 'Contact Me' page from your website. Just install email application on your phone and respond to emails on the go.`
   },
   {
     bookmark: true,
     icon: 'ri:lock-2-line',
     above: '$ 0.00',
     title: 'Free HTTPS',
-    description: 'Ever had to pay for SSL certificates? Not anymore. All of your web traffic is secured using Let’s Encrypt certificate. This ensures that all transmitted data is safe over internet from prying eyes.'
+    description: `Ever had to pay for SSL certificates? Not anymore. All of your web traffic is secured using Let's Encrypt certificate. This ensures that all transmitted data is safe over internet from prying eyes.`
   },
   {
     bookmark: false,
