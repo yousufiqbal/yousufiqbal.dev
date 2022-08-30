fc68d7a0d36e6108a62b5e1752f6ce1970961f88
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-08-30 10:11:39 -0700

    removed all validator added regexes

--- src/lib/validations.js
+++ src/lib/validations.js
@@ -1,35 +1,42 @@
-import v from 'validator'
 import { trimObject } from './utils'
+let pattern = {
+  email: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  alpha: /^[a-zA-Z\s]+$/,
+  numeric: /^\d+$(\.\d+)?/
+}
+
+let Form = { name: '', email: '', whatsapp: '', message: '' }
 
 /**
  * 
- * @param {{ name: '', email: '', whatsapp: '', message: '' }} form 
+ * @param {Form} rawForm 
  * @returns errors object (empty object in no errors)
  */
-export const validateContactForm = form => {
+export const validateContactForm = rawForm => {
   /** @type {Form} */
   let errors = {}
-  let form = trimObject(form)
+  /** @type {Form} */
+  let form = trimObject(rawForm)
 
   // Name
   if (!form.name) {
     errors.name = 'Required'
   }
-  else if (!v.isAlpha(form.name)) {
-    errors.name = 'Name should only contain letters'
-  } else if (!v.isLength(form.name, { max: 50, min: 1})) {
+  else if (!pattern.alpha.test(form.name)) {
+    errors.name = 'Name should only contain alphabets and spaces'
+  } else if (form.name.length <= 1 || form.name.length >= 50) {
     errors.name = 'Name should be between 1 to 50 characters in length'
   }
 
   // Email
   if (!form.email) {
     errors.email = 'Required'
-  } else if (!v.isEmail(form.email)) {
+  } else if (!pattern.email.test(form.email)) {
     errors.email = 'Email is invalid'
   }
 
   // WhatsApp
-  if (form.whatsapp && !v.isNumeric(form.whatsapp)) {
+  if (form.whatsapp && !pattern.numeric.test(form.whatsapp)) {
     errors.whatsapp = 'Invalid Whatsapp Number'
   }
 
@@ -37,10 +44,10 @@ export const validateContactForm = form => {
   if (!form.message) {
     errors.message = 'Required'
   }
-  else if (!v.isLength(form.message, {min: 10, max: 5000})) {
+  else if (form.message.length <= 10 || form.message.length >= 5000) {
     errors.message = 'Message should be between 10 and 5000 characters in length'
   }
 
   return errors
-  
+
 }
