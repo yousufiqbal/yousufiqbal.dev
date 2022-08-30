ff82ea4a4d35be66d7e553ec56d8753bc9adf8dd
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-08-30 14:18:10 +0500

    added diesgn message

--- src/routes/designs/+page.svelte
+++ src/routes/designs/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-  import Section from "$lib/components/Section.svelte";
+  import Content from "$lib/components/Content.svelte";
+import Section from "$lib/components/Section.svelte";
 import Seo from "$lib/components/Seo.svelte";
   import Title from "$lib/components/Title.svelte";
 </script>
@@ -8,4 +9,7 @@ import Seo from "$lib/components/Seo.svelte";
 
 <Section>
   <Title typeEffect icon="ri:brush-line" title="Designs" above="All Original Artwork" />
+  <Content>
+    <p>This website is just released, and hence is a work in progress. Stay tuned for designs you may see them in a week or two. Take care till then!</p>
+  </Content>
 </Section>
