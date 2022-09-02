0606dc60517a5969663287050e6f5887e343969a
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-09-02 11:25:27 -0700

    added-clinic-template

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -2,5 +2,6 @@
 // Link: {url}.yousufiqbal.dev
 
 export const templates = [
-  { name: 'Rayhan Minima', type: 'personal', url: 'rayhan-minima' }
+  { name: 'Rayhan Minima', type: 'personal', url: 'rayhan-minima' },
+  { name: 'Clinic', type: 'medical', url: 'clinic-template' },
 ]
