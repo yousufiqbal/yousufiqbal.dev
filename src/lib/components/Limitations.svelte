68a8ad5bf7fe66cf4e5aed1d4fa5cc10f842c185
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-09-04 13:09:48 -0700

    .. textual

--- src/lib/components/Limitations.svelte
+++ src/lib/components/Limitations.svelte
@@ -12,9 +12,9 @@
     <li>When pages limit surpass, subsequent pages will be charged $&nbsp;{price}.00 per page</li>
     <li>Free email is for one account only</li>
     <li>Contact Form is a page where users interested can send you a message. You receive this message inside your custom domain email which is provided for free. (Not included in the basic plan)</li>
-    <li>Free hosting, email and SSL certificates are vendors supplied. If one vendor abandon them, I'll switch to second and so on. If all of the vendors anandon them. Then I've no choice but to shift you to a paid plan for hosting / email / SSL after consultation from you.</li>
-    <li>The template you chose for the design of your website is copyrighted artwork which means you cannot sell template design to someone else. If you want template ownership, contact me and I can design one for you on your ownership, but this increases pricing.</li>
-    <li>The build generated from your website source code is completely yours. You are its owner, but the source code isn’t. There is not much of a need for common users to buy source code. But still, if you want to buy source code and own it. You can contact me.</li>
+    <li>Free hosting, email and SSL certificates are vendors supplied. If one vendor abandon them, I'll switch to second and so on. If all of the vendors abandon them. Then I've no choice but to shift you to a paid plan for hosting / email / SSL after consultation from you.</li>
     <li>Contact Page can send upto 300 messages per day, depending upon the reputation of your email. If you surpass this limit which is less likely, then you have to subscribe one of paid options like SendGrid, Amazon SESm etc.</li>
+    <li>The build-code generated from your website source code is completely yours. You are its owner, but the source code isn't. There is not much of a need for common users to buy source code. But still, if you want to buy source code and own it. You can contact me.</li>
+    <li>The template you chose for the design of your website, becomes copyrighted work post-design, which means you cannot sell template design to someone else. If you want template ownership, contact me and I can sell it to you for your ownership, but this increases pricing.</li>
   </ol>
 </Content>
--- src/routes/(app)/templates/+page.svelte
+++ src/routes/(app)/templates/+page.svelte
@@ -1,18 +1,16 @@
 <script>
 import Container from "$lib/components/Container.svelte";
-  import Content from "$lib/components/Content.svelte";
-  import Section from "$lib/components/Section.svelte";
   import Seo from "$lib/components/Seo.svelte";
   import Templates from "$lib/components/Templates.svelte";
   import Title from "$lib/components/Title.svelte";
 </script>
 
-<Seo title="Templates" description="All artwork web templates, developed to jump start your website from any given web template." />
+<Seo title="Templates" description="Best template designs to jump start your website from any given web template." />
 
 <Container --pd="50px 30px">
   <Title typeEffect icon="ri:layout-2-line" title="Templates" above="Websites Ideas" />
-  <Content>
+  <!-- <Content>
     <p>This website is just released, and hence is a work in progress. Stay tuned for templates you may see them in a week or two. Take care till then!</p>
-  </Content>
+  </Content> -->
   <Templates />
 </Container>
