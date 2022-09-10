0b9a26a5586794e1543fb48a1664201f4768cce6
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-09-09 22:19:38 -0700

    services wip..

--- src/lib/data.js
+++ src/lib/data.js
@@ -102,20 +102,61 @@ export let technologies = [
 export let services = [
   {
     icon: 'ri:building-2-line',
-    name: 'Business Website Development',
-    href: 'business-website',
-    description: 'Business websites provide the best way to share you product and services with open world. Showcase your products & services so interested people can contact.',
+    name: 'Website Development',
+    href: 'website-development',
+    description: 'Websites provide the best way to share you product and services with open world, so interested people can contact. There are three plans for  Website development',
+    packages: [
+      {
+        name: 'Basic',
+        attributes: {
+          'Pages': '1 Page',
+          'Customization': 'No',
+          'Web Hosting': 'Free (100GB)',
+          'SSL Certificate': 'Free',
+          'Content Upload': 'Free',
+          'Custom Email': 'Free (5GB)',
+          'Contact Form': 'No',
+          'Price': '$ 50.00',
+        }
+      },
+      {
+        name: 'Standard',
+        attributes: {
+          'Pages': '5 Pages',
+          'Customization': 'Mini',
+          'Web Hosting': 'Free (100GB)',
+          'SSL Certificate': 'Free',
+          'Custom Email': 'Free (5GB)',
+          'Content Upload': 'Free',
+          'Contact Form': 'Yes',
+          'Price': '$ 100.00',
+        }
+      },
+      {
+        name: 'Premium',
+        attributes: {
+          'Pages': '10 Pages',
+          'Customization': 'Mini',
+          'Web Hosting': 'Free (100GB)',
+          'SSL Certificate': 'Free',
+          'Custom Email': 'Free (5GB)',
+          'Content Upload': 'Free',
+          'Contact Form': 'Yes',
+          'Price': '$ 150.00',
+        }
+      },
+    ]
   },
-  {
-    icon: 'ri:user-star-line',
-    name: 'Personal Website Development',
-    href: 'personal-website',
-    description: 'Personal websites are the best way to introduce your profession. Showcase your education, skills, experiences so interested people can contact.',
-  },
-  {
-    icon: 'ri:arrow-go-forward-line',
-    name: 'Design Files to Live Website Conversion',
-    href: 'design-to-website',
-    description: 'Converting design files to live websites always pleases me. Your design file can be from any vector software including but not limited to Figma, XD, Illustrator or Sketch.',
-  },
+  // {
+  //   icon: 'ri:shopping-cart-line',
+  //   name: 'E-Commerce Website',
+  //   href: 'design-to-website',
+  //   description: 'Converting design files to live websites always pleases me. Your design file can be from any vector software including but not limited to Figma, XD, Illustrator or Sketch.',
+  // },
+  // {
+  //   icon: 'ri:arrow-go-forward-line',
+  //   name: 'Design Files to Live Website Conversion',
+  //   href: 'design-to-website',
+  //   description: 'Converting design files to live websites always pleases me. Your design file can be from any vector software including but not limited to Figma, XD, Illustrator or Sketch.',
+  // },
 ]
