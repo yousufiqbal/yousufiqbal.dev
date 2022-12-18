37a012241d261e6978632936ae523ca64e13573f
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-12-18 14:50:10 +0500

    removed icons

--- src/lib/data/services.js
+++ src/lib/data/services.js
@@ -1,26 +1,21 @@
 export const services = [
   {
-    icon: 'i-ri:home-line',
     name: 'Business / Company Website',
     description: 'Showcasing the services you provide or products you sell, testimonials, location of your office, etc. to the digital world is quite needy nowadays. Business or company websites serves this purpose at its best.',
   },
   {
-    icon: 'i-ri:home-line',
     name: 'E-Commerce Website',
     description: 'Whether you manufacture or buy-and-sell, "e-commerce" websites are best way of selling products and earning. Keeping performance and seo in mind. E-commerce websites are designed to sell products effectively and efficiently.',
   },
   {
-    icon: 'i-ri:home-line',
     name: 'Custom Web Application',
     description: 'If you have an idea, popped out to you only, and you want to digitalize it by making a web app, then, I\'m at your service. After extensive documentation and discussion. I can (AlhamduLILLAH) design a custom web-app for you at custom rates.',
   },
   {
-    icon: 'i-ri:home-line',
     name: 'Personal Website',
     description: 'Personal website allows you to potray your personality, including but not limited to, your education, portfolio, experiences & testimonials. It\'s like a digital resume with interactivity.',
   },
   {
-    icon: 'i-ri:home-line',
     name: 'Non-Profit Website',
     description: 'If you need a website for a non-profit company or group busy in Muslim welfare. I can design your project for free after field work discussions.',
   },
