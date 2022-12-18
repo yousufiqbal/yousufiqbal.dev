c536e71019c4692fa9c7d0f224cf23f55d8c0675
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-12-18 12:54:29 +0500

    changed order

--- src/lib/data/technologies.js
+++ src/lib/data/technologies.js
@@ -1,5 +1,13 @@
 export const technologies = [
   {
+    name: 'SvelteJS',
+    description: 'Svelte is a radical new approach to building user interfaces. Whereas traditional frameworks like React and Vue do the bulk of their work in the browser, Svelte shifts that work into a compile step.',
+  },
+  {
+    name: 'SvelteKit',
+    description: 'SvelteKit doesn\'t compromise on SEO, progressive enhancement or the initial load experience — but unlike traditional server-rendered apps, navigation is instantaneous for that app-like feel.',
+  },
+  {
     name: 'HTML',
     description: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.',
   },
@@ -18,14 +26,6 @@ export const technologies = [
   {
     name: 'MySQL',
     description: 'MySQL is an open-source relational database management system. As with other relational databases, MySQL stores data in tables made up of rows and columns.',
-  },
-  {
-    name: 'SvelteJS',
-    description: 'Svelte is a radical new approach to building user interfaces. Whereas traditional frameworks like React and Vue do the bulk of their work in the browser, Svelte shifts that work into a compile step.',
-  },
-  {
-    name: 'SvelteKit',
-    description: 'SvelteKit doesn\'t compromise on SEO, progressive enhancement or the initial load experience — but unlike traditional server-rendered apps, navigation is instantaneous for that app-like feel.',
   },
   {
     name: 'VS Code',
